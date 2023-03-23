875435787f0a67dc7bdabd2c085d50a2b1f878bf
timothee-dlh <108541810+timothee-dlh@users.noreply.github.com>
2023-03-24 00:26:29 +0100

    Create sev_seg.h

---
+++ sev_seg.h
@@ -0,0 +1,19 @@
+void convert(int number);
+
+void one(int digit);
+void two(int digit);
+void three(int digit);
+void four(int digit);
+void five(int digit);
+void six(int digit);
+void seven(int digit);
+void eight(int digit);
+void nine(int digit);
+
+
+void output( int D1, int D2, int D3, int D4, int DP, int a, int b, int c, int d, int e, int f, int g);
+void output( int D1, int D2, int D3, int D4, int a, int b, int c, int d, int e, int f, int g);
+
+void pin(int one, int two, int three, int four, int five, int six, int seven, int D1, int D2, int D3, int D4);
+
+void write(int nombre1);
