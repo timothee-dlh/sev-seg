2579d563a7aa05a607e2315a5094ac1e89cc68ce
timothee-dlh <108541810+timothee-dlh@users.noreply.github.com>
2023-03-24 01:04:39 +0100

    Update sev_seg.h

--- sev_seg.h
+++ sev_seg.h
@@ -12,7 +12,6 @@ void nine(int digit);
 
 
 void output( int D1, int D2, int D3, int D4, int DP, int a, int b, int c, int d, int e, int f, int g);
-void output( int D1, int D2, int D3, int D4, int a, int b, int c, int d, int e, int f, int g);
 
 void pin(int one, int two, int three, int four, int five, int six, int seven, int D1, int D2, int D3, int D4);
 
